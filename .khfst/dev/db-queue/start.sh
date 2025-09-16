18036bfa043bc9158c142722f5d69881b116ebd7
LJ S.A. <lvjhn.d@gmail.com>
2025-09-16 19:59:28 +0800

    added other databases

---
+++ .khfst/dev/db-queue/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+cp /home/postgres/raw-certs/* /home/postgres/certs/
+chown postgres:postgres /home/postgres/certs/*
+chmod 600 /home/postgres/certs/*.key
+chmod 644 /home/postgres/certs/*.crt
+
+export POSTGRES_DB=$QUEUE_DB_NAME
+export POSTGRES_USER=$QUEUE_DB_USER
+export POSTGRES_PASSWORD=$QUEUE_DB_PASSWORD
+
+# Start postgres in background
+docker-entrypoint.sh postgres \
+  -c ssl=on \
+  -c ssl_cert_file=/home/postgres/certs/db-queue.crt \
+  -c ssl_key_file=/home/postgres/certs/db-queue.key \
+  -c ssl_ca_file=/home/postgres/certs/ca.crt 
